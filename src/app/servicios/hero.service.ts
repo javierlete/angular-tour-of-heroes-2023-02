024cfc1ac1dcd092a7abfc812abe6404e7448988
Javier Lete <jglete@eskura21.com>
2023-02-09 20:02:38 +0100

    Gestión de errores de conexión y mensajes

--- src/app/servicios/hero.service.ts
+++ src/app/servicios/hero.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { Hero } from '../modelos/hero';
 import { MessageService } from './message.service';
 
@@ -13,35 +13,68 @@ export class HeroService {
   constructor(private http: HttpClient, private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(_ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+      // catchError(err => {
+      //   this.log('ERROR AL PEDIR LOS CLIENTES ' + err.statusText);
+      //   return of([]);
+      // }))
+    );
   }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return this.http.get<Hero>(this.heroesUrl + '/' + id);
+    return this.http.get<Hero>(this.heroesUrl + '/' + id).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>('getHero'))
+    );
   }
 
   searchHeroes(term: string): Observable<Hero[]> {
     if (!term.trim()) {
       return of([]);
     }
-    
-    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`);
+
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`search hero ${term}`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
   }
 
   addHero(hero: Hero): Observable<Hero> {
-    this.messageService.add(`HeroService: added hero`);
-    return this.http.post<Hero>(this.heroesUrl, hero);
+
+    return this.http.post<Hero>(this.heroesUrl, hero).pipe(
+      tap(_ => this.log(`added hero`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
   }
 
   updateHero(hero: Hero): Observable<Hero> {
-    this.messageService.add(`HeroService: updated hero id=${hero.id}`);
-    return this.http.put<Hero>(this.heroesUrl, hero);
+    return this.http.put<Hero>(this.heroesUrl, hero).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<Hero>('updateHero'))
+    );
   }
 
   deleteHero(id: number): Observable<any> {
-    this.messageService.add(`HeroService: deleted hero id=${id}`);
-    return this.http.delete<any>(this.heroesUrl + '/' + id);
+
+    return this.http.delete<any>(this.heroesUrl + '/' + id).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<any>('deleteHero'))
+    );
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
   }
 }
